de1b21298a2a461a04f09bf161b0f8ab7d08b80e
alexeerma <aleksander.eerma@hotmail.com>
2022-09-21 16:36:10 +0300

    Secon exercise done, exept 1 test where throw error if when number is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(data, times) {
+    let string = '';
+    for (let i = 1; i <= times; i++) {
+        string += data;
+        
+    }
+      return (string);
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
