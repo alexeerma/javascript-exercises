fda3905ad307587f6595731c9079be2ded7d82b0
alexeerma <aleksander.eerma@hotmail.com>
2022-09-22 17:22:40 +0300

    Reverse string done, interesting solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+        return string.split('').reverse().join('');
+    
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// Splitting string into an array
+// Reversing array values
+// concatenating values into string 
+// returning the reversed value
+
+/* 
+    let string = 'hello';
+    
+    const splitArray = string.split(" ");
+    const reverseArray = splitArray.reverse();
+    const joinArray =reverseArray.join(" ");
+    return joinArray; 
+
+*/
