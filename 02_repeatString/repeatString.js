629565a3c318db43d5734f1b66fff22c3b27634f
alexeerma <aleksander.eerma@hotmail.com>
2022-09-22 17:00:28 +0300

    Fixed the Error issue

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,11 @@
 const repeatString = function(data, times) {
+    if (times < 0) return 'ERROR';
     let string = '';
     for (let i = 1; i <= times; i++) {
         string += data;
         
     }
-      return (string);
+      return string;
 };
 
 
