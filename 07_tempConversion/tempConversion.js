8f1c96004614b37860d976fa8db1cb0d757e39f5
alexeerma <aleksander.eerma@hotmail.com>
2022-09-23 16:45:08 +0300

    Conversion to celsius and fahrenheit done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    let value = ((fahrenheit - 32) * (5/9));
+    var roundedValue = (Math.round(value * 10) / 10);
+    return roundedValue;
 };
 
-const ctof = function() {
 
+const ctof = function(celsius) {
+    let value2 = (celsius * (9/5) + 32);
+    var roundedValue2 = (Math.round(value2 * 10)/ 10);
+    return roundedValue2;
 };
 
 // Do not edit below this line
@@ -11,3 +16,19 @@ module.exports = {
   ftoc,
   ctof
 };
+
+/* FTOC 
+store fahrenheit
+(F-32) * 5/9 
+decimal point conversion
+return the value
+
+
+*/
+
+/* CTOF 
+store fahrenheit
+C * (9/5) + 32 
+decimal point conversion
+return the value
+*/
