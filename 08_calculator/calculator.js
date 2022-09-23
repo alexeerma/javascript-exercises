802931d31e700794a144e76433f2aac467ea3ec4
alexeerma <aleksander.eerma@hotmail.com>
2022-09-23 17:22:06 +0300

    Calculator all 16 tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+    let sumAdd = num1 + num2; 
+    return sumAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+    let sumSub = num1 - num2;
+    return sumSub;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  
+  sumSum = arr.reduce((a, b) => a + b, 0);
+    return sumSum;
+
+     
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    sumMply = arr.reduce((a, b) => a * b, 1);
+    return sumMply;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+    let sumPwr = num1 ** num2;
+    return sumPwr;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
+// This is another implementation of Factorial that uses recursion
+// THANKS to @ThirtyThreeB!
+/* const recursiveFactorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial (n-1); 
+};*/
 
 // Do not edit below this line
 module.exports = {
