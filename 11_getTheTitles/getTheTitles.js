1756ce7d0ea4564d94075dc6e203ee3d851d2b0a
alexeerma <aleksander.eerma@hotmail.com>
2022-09-24 15:50:29 +0300

    Pesudocode done, will continue later

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -4,3 +4,17 @@ const getTheTitles = function() {
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+/* 
+array
+array has 2 values
+function needs to get the titles of values
+set the array
+extract only the title values from array
+return the values in a new array.
+
+
+
+
+*/
