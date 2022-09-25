eda61dcbc708b71d8843dc18636f4684f86929ff
alexeerma <aleksander.eerma@hotmail.com>
2022-09-25 16:47:08 +0300

    Did the DOM, except Css styling

---
+++ exampleExc/script.js
@@ -0,0 +1,50 @@
+
+const container = document.querySelector('.container');
+
+const para = document.createElement('p');
+
+para.textContent = "Hey I'm Red!";
+para.classList.add('para1');
+container.appendChild(para);
+
+// Adding the h3
+
+const titl = document.createElement('h3');
+titl.classList.add('titl-css');
+titl.textContent = 'Im blue h3!';
+container.appendChild(titl);
+
+//adding a div with different info inside (h1,p,)
+
+const content = document.createElement('div');
+content.classList.add('content');
+const titl2 = document.createElement('h1');
+const para2 = document.createElement('p')
+titl2.textContent = 'Im in a div!';
+para2.textContent = 'ME TOO!';
+content.appendChild(titl2);
+content.appendChild(para2);
+container.appendChild(content);
+
+//adding the css
+var stiil = document.getElementsByClassName('titl-css');
+stiil.innerHTML = "h3 {color:blue;}";
+content.appendChild(sheet);
+
+
+
+
+
+
+
+
+
+/* const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content); */
+
+
